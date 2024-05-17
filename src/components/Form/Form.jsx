e58bbd82b3134ea9ec91923aecf7fd8aab229921
v.belyakov <v.belyakov@ritg.ru>
2024-05-17 15:03:16 +0400

    Правка onSendData

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect} from 'react';
 import './Form.css';
 import {useTelegram} from "../../hooks/useTelegram";
 
@@ -8,6 +8,22 @@ const Form = () => {
     const [subject, setSubject] = React.useState('physical');
 
     const {tg} = useTelegram();
+
+    const onSendData = useCallback( () => {
+        const data = {
+            country,
+            street,
+            subject
+        }
+        tg.sendData(JSON.stringify(data))
+    })
+
+    useEffect(() => {
+        tg.Telegram.WebApp.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.Telegram.WebApp.offEvent('mainButtonClicked', onSendData);
+        }
+    }, [])
 
     useEffect(() => {
         tg.MainButton.setParams({
